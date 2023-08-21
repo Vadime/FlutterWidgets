5c73ae3f7678f299c934bf9409c7dfdd62fd351c
Vadime <vadime.novikau@icloud.com>
2023-08-21 13:54:45 +0200

    added authentication controller

---
+++ utils/lib/controllers/authentication_controller.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AuthenticationController extends Cubit<bool> {
+  AuthenticationController() : super(false);
+
+  void login() => emit(true);
+
+  void logout() => emit(false);
+
+  static AuthenticationController of(context) =>
+      BlocProvider.of<AuthenticationController>(context);
+
+  
+}
