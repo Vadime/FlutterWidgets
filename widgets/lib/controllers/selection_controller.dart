98bc00ee3e91d891ae0ceb55bcc9a40fb5e8759f
Vadime <vadime.novikau@icloud.com>
2023-08-27 16:58:24 +0200

    added listener to selection controller

--- widgets/lib/controllers/selection_controller.dart
+++ widgets/lib/controllers/selection_controller.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:widgets/widgets.dart';
 
 abstract class SelectionController<T, S> extends Cubit<T> {
+  List<VoidCallback> listerners = [];
   SelectionController(super.initialState);
 
   void toggle(S value);
@@ -9,6 +12,17 @@ abstract class SelectionController<T, S> extends Cubit<T> {
   @override
   void onChange(Change<T> change) {
     super.onChange(change);
+    for (var element in listerners) {
+      element();
+    }
     Logging.logDetails(runtimeType.toString(), change);
+  }
+
+  void addListener(VoidCallback listener) {
+    listerners.add(listener);
+  }
+
+  void dispose() {
+    listerners.clear();
   }
 }
