44c2fc60df9a62c83ba3c698a1308cb2aeb7d8bb
Vadime <vadime.novikau@icloud.com>
2023-08-28 13:29:47 +0200

    image compression implemented

--- widgets/lib/utils/file_picking.dart
+++ widgets/lib/utils/file_picking.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:image_compression_flutter/image_compression_flutter.dart';
+import 'package:widgets/widgets.dart';
 
 class FilePicking {
   static Future<File?> pickImage() async {
@@ -8,15 +10,42 @@ class FilePicking {
 
     try {
       result = await FilePicker.platform.pickFiles(
+        dialogTitle: 'Select an image',
         type: FileType.image,
         withData: true,
       );
       if (result != null && result.files.single.path != null) {
-        return File(result.files.single.path!);
+        final file = result.files.single;
+
+        if (file.path == null && file.bytes == null) return null;
+
+        ImageFile input =
+            ImageFile(filePath: file.path!, rawBytes: file.bytes!);
+
+        final image = await compressImage(input);
+
+        return File(image.filePath);
       }
     } catch (e) {
       return null;
     }
     return null;
+  }
+
+  static Future<ImageFile> compressImage(ImageFile input) async {
+    Logging.log('Input size : ${input.sizeInBytes}');
+    Configuration config = const Configuration(
+      outputType: ImageOutputType.webpThenJpg,
+      // can only be true for Android and iOS while using ImageOutputType.jpg or ImageOutputType.pngÏ
+      useJpgPngNativeCompressor: false,
+      // set quality between 0-100
+      quality: 40,
+    );
+
+    final param = ImageFileConfiguration(input: input, config: config);
+    final output = await compressor.compress(param);
+
+    Logging.log('Output size : ${output.sizeInBytes}');
+    return output;
   }
 }
